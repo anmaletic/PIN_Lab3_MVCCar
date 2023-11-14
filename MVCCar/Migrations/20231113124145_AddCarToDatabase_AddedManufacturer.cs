5eaf72f7d17398e4f12ea6aeb6b511c6ea31c084
AnMal <junkie_@msn.com>
2023-11-14 06:54:27 +0100

    Add project files.

---
+++ MVCCar/Migrations/20231113124145_AddCarToDatabase_AddedManufacturer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCCar.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCarToDatabase_AddedManufacturer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Manufacturer",
+                table: "Car",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Manufacturer",
+                table: "Car");
+        }
+    }
+}
---
+++ MVCCar/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MVCCar.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+
+        [DisplayName("Production Year")]
+        public int ProductionYear { get; set; }
+        public decimal Price { get; set; }
+    }
+}
